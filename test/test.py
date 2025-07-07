e1fc65a921045d195a3b622a8bb474a16250eac2
ShangChien <chenshangqian@dp.tech>
2025-07-07 15:53:02 +0800

    modify:test.py

--- test/test.py
+++ test/test.py
@@ -9,7 +9,7 @@ from tools.base import (
     InputPK,
     RES
 )
-from tools.nmr import NMR_predict,SearchInput,PredictInput,ReversePredictInput,InputNMR,Result,TaskSubmit
+from tools.nmr import NMR_search, NMR_predict,NMR_reverse_predict,SearchInput,PredictInput,ReversePredictInput,InputNMR,Result,TaskSubmit
 import asyncio
 
 input_data = InputData(smiles="CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
@@ -18,9 +18,9 @@ input_pk = InputPK(smiles="CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
 async def test():
     start = time.time()
     tasks = [
-        # ADMET_predict(input_data),
-        NMR_predict(PredictInput(smiles_list=["CN1C=NC2=C1C(=O)N(C(=O)N2C)C"])),
-        # ToxScan_predict(input_data)
+        #NMR_search(SearchInput(C_shifts=[19.1, 19.4, 23.5, 62.7, 126.3, 126.5, 127.0, 128.36, 128.41, 130.2, 130.8, 134.8, 135.98, 136.02, 138.9])),
+        #NMR_predict(PredictInput(smiles_list=["CN1C=NC2=C1C(=O)N(C(=O)N2C)C"])),
+        NMR_reverse_predict(ReversePredictInput(C_shifts=[19.1, 19.4, 23.5, 62.7, 126.3, 126.5, 127.0, 128.36, 128.41, 130.2, 130.8, 134.8, 135.98, 136.02, 138.9])),
     ]
     res_list:list[RES] = await asyncio.gather(*tasks)
     print(f"time: {time.time() - start}")
